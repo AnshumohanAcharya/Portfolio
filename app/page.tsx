588baa61660bdb745342556fd867753412aa48c4
Anshumohan Acharya <anshumohanacharya19@gmail.com>
2025-08-24 20:30:20 +0530

    Portfolio V2 - Inital Setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from '@/components/theme/mode-toggle';
+
+export default function Home() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <ModeToggle />
+    </div>
+  );
+}
